3bc60d27bb61455bbc49be4a73d15eb86b822722
oscarlindenbaum <56974866+oscarlindenbaum@users.noreply.github.com>
2020-04-07 14:52:54 +0100

    removed iframe around app

--- develop/f31abb6ac893e34e380d74ca43542f0a56e605b1/cook_safe/flutter_service_worker.js
+++ develop/f31abb6ac893e34e380d74ca43542f0a56e605b1/cook_safe/flutter_service_worker.js
@@ -4,7 +4,7 @@ const RESOURCES = {
   "assets/AssetManifest.json": "801125fca546f506babac9af4ec061ba",
 "assets/FontManifest.json": "18eda8e36dfa64f14878d07846d6e17f",
 "assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"assets/LICENSE": "945b1b106e41eaa8243326b33069010f",
+"assets/LICENSE": "077aac2ba39001da39e4f4d74b13da54",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
 "assets/packages/font_awesome_flutter/lib/fonts/fa-brands-400.ttf": "5a37ae808cf9f652198acde612b5328d",
 "assets/packages/font_awesome_flutter/lib/fonts/fa-regular-400.ttf": "2bca5ec802e40d3f4b60343e346cedde",
@@ -13,7 +13,8 @@ const RESOURCES = {
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "525d80c5907260123241c058e7b682d5",
-"main.dart.js": "a3efc038f7a0907e0b6de973e05c1cf6",
+"/": "525d80c5907260123241c058e7b682d5",
+"main.dart.js": "2f437e1077c7676f7865c4d1fad4cefa",
 "manifest.json": "e62ca04c9a04b04227494232a88ade2e"
 };
 
