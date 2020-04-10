6743a79611472d5815b7f79aea6a33d15da0b66a
oscarlindenbaum <56974866+oscarlindenbaum@users.noreply.github.com>
2020-04-10 16:43:00 +0100

    ui refresh build v3.3.1

--- develop/f31abb6ac893e34e380d74ca43542f0a56e605b1/cook_safe_moderator/flutter_service_worker.js
+++ develop/f31abb6ac893e34e380d74ca43542f0a56e605b1/cook_safe_moderator/flutter_service_worker.js
@@ -1,17 +1,18 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
+  "assets/AssetManifest.json": "f444ee4c97fc91245180cefa9283f970",
 "assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
 "assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"assets/LICENSE": "168bb05de726358ad11925da4351cc15",
+"assets/images/defaultAvatar.jpg": "d5f51047d8bd6327ec4a74361a7aae7f",
+"assets/LICENSE": "ffa2d92865a6cd4dec1533e92c574356",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "4e8378f8caefa888084d30c5368604ec",
 "/": "4e8378f8caefa888084d30c5368604ec",
-"main.dart.js": "62ecb346ca6a2d43963af926f7d07f0a",
+"main.dart.js": "df87f6add2b49e0d05bd35f344369455",
 "manifest.json": "bb75104b3c6d5217a82fd44ac2dd5b2c"
 };
 
