3caedb3f0d011137ba7bedc7846942038748c922
oscarlindenbaum <56974866+oscarlindenbaum@users.noreply.github.com>
2020-04-11 19:15:08 +0100

    put actual user files up

--- develop/f31abb6ac893e34e380d74ca43542f0a56e605b1/cook_safe/flutter_service_worker.js
+++ develop/f31abb6ac893e34e380d74ca43542f0a56e605b1/cook_safe/flutter_service_worker.js
@@ -1,19 +1,22 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "f444ee4c97fc91245180cefa9283f970",
-"assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
+  "assets/AssetManifest.json": "ae47246e97ea965d140e1c7ac8e35e6a",
+"assets/assets/recipes.json": "2183b454e8b76ef0ddbe27ae1095d094",
+"assets/FontManifest.json": "18eda8e36dfa64f14878d07846d6e17f",
 "assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"assets/images/defaultAvatar.jpg": "d5f51047d8bd6327ec4a74361a7aae7f",
-"assets/LICENSE": "22b87a0dce2ebc471b99ba6e43cd959c",
+"assets/LICENSE": "5d8b67099f4ff4e4bd8a8bffc07c2da4",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
+"assets/packages/font_awesome_flutter/lib/fonts/fa-brands-400.ttf": "5a37ae808cf9f652198acde612b5328d",
+"assets/packages/font_awesome_flutter/lib/fonts/fa-regular-400.ttf": "2bca5ec802e40d3f4b60343e346cedde",
+"assets/packages/font_awesome_flutter/lib/fonts/fa-solid-900.ttf": "2aa350bd2aeab88b601a593f793734c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "4e8378f8caefa888084d30c5368604ec",
-"/": "4e8378f8caefa888084d30c5368604ec",
-"main.dart.js": "25ef12b931424af704ce1ee6b2572ea6",
-"manifest.json": "bb75104b3c6d5217a82fd44ac2dd5b2c"
+"index.html": "b9662310a75fd3b7a4702077fb434a56",
+"/": "b9662310a75fd3b7a4702077fb434a56",
+"main.dart.js": "717e02fc3ad1e8bd7fc28c42b77d6fdc",
+"manifest.json": "e62ca04c9a04b04227494232a88ade2e"
 };
 
 self.addEventListener('activate', function (event) {
