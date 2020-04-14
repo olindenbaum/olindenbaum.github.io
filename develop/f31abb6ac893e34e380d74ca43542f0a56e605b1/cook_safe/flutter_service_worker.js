e6fc5a657c14dfcd3d5ecf6aabbdc8adc0bce558
oscarlindenbaum <56974866+oscarlindenbaum@users.noreply.github.com>
2020-04-14 19:13:21 +0100

    user dynamic sizing v3.4.5+3

--- develop/f31abb6ac893e34e380d74ca43542f0a56e605b1/cook_safe/flutter_service_worker.js
+++ develop/f31abb6ac893e34e380d74ca43542f0a56e605b1/cook_safe/flutter_service_worker.js
@@ -1,7 +1,9 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "ae47246e97ea965d140e1c7ac8e35e6a",
+  "assets/AssetManifest.json": "11c8de92bd379c12274c21636daf9842",
+"assets/assets/favicon.ico": "6fa40b18241f63bc08805ac763ac91b9",
+"assets/assets/logo.png": "76b79618432ea176e66cec0ce624be63",
 "assets/assets/recipes.json": "2183b454e8b76ef0ddbe27ae1095d094",
 "assets/FontManifest.json": "18eda8e36dfa64f14878d07846d6e17f",
 "assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
@@ -10,12 +12,12 @@ const RESOURCES = {
 "assets/packages/font_awesome_flutter/lib/fonts/fa-brands-400.ttf": "5a37ae808cf9f652198acde612b5328d",
 "assets/packages/font_awesome_flutter/lib/fonts/fa-regular-400.ttf": "2bca5ec802e40d3f4b60343e346cedde",
 "assets/packages/font_awesome_flutter/lib/fonts/fa-solid-900.ttf": "2aa350bd2aeab88b601a593f793734c0",
-"favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"favicon.png": "100b7d193a942c4c53888bdb2de3d26f",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "b9662310a75fd3b7a4702077fb434a56",
 "/": "b9662310a75fd3b7a4702077fb434a56",
-"main.dart.js": "f4c1e1f367ede23fa38b3dfe7cafce11",
+"main.dart.js": "01183b01ba30ecfed8cac2c57af309f3",
 "manifest.json": "e62ca04c9a04b04227494232a88ade2e"
 };
 
