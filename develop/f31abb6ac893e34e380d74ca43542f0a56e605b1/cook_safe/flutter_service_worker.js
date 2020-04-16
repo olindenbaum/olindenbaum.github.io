b88904dc589a97787d78d2a0af38bf07ce95efe4
oscarlindenbaum <56974866+oscarlindenbaum@users.noreply.github.com>
2020-04-16 12:53:33 +0100

    build 3.4.5+4

--- develop/f31abb6ac893e34e380d74ca43542f0a56e605b1/cook_safe/flutter_service_worker.js
+++ develop/f31abb6ac893e34e380d74ca43542f0a56e605b1/cook_safe/flutter_service_worker.js
@@ -17,7 +17,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "b9662310a75fd3b7a4702077fb434a56",
 "/": "b9662310a75fd3b7a4702077fb434a56",
-"main.dart.js": "01183b01ba30ecfed8cac2c57af309f3",
+"main.dart.js": "74c8c4d10527c84dce6e4d57c12cae63",
 "manifest.json": "e62ca04c9a04b04227494232a88ade2e"
 };
 
--- develop/f31abb6ac893e34e380d74ca43542f0a56e605b1/cook_safe_moderator/oldversion/flutter_service_worker.js
+++ develop/f31abb6ac893e34e380d74ca43542f0a56e605b1/cook_safe_moderator/oldversion/flutter_service_worker.js
@@ -1,24 +1,20 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "11c8de92bd379c12274c21636daf9842",
-"assets/assets/favicon.ico": "6fa40b18241f63bc08805ac763ac91b9",
-"assets/assets/logo.png": "76b79618432ea176e66cec0ce624be63",
-"assets/assets/recipes.json": "2183b454e8b76ef0ddbe27ae1095d094",
-"assets/FontManifest.json": "18eda8e36dfa64f14878d07846d6e17f",
+  "assets/AssetManifest.json": "9deffc3895ad49ed383b0f3fb108956f",
+"assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
 "assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"assets/LICENSE": "5d8b67099f4ff4e4bd8a8bffc07c2da4",
+"assets/images/defaultAvatar.jpg": "d5f51047d8bd6327ec4a74361a7aae7f",
+"assets/images/favicon.ico": "6fa40b18241f63bc08805ac763ac91b9",
+"assets/LICENSE": "6fc1ab00c9e41d4cb9de92d712f6ec81",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
-"assets/packages/font_awesome_flutter/lib/fonts/fa-brands-400.ttf": "5a37ae808cf9f652198acde612b5328d",
-"assets/packages/font_awesome_flutter/lib/fonts/fa-regular-400.ttf": "2bca5ec802e40d3f4b60343e346cedde",
-"assets/packages/font_awesome_flutter/lib/fonts/fa-solid-900.ttf": "2aa350bd2aeab88b601a593f793734c0",
 "favicon.png": "100b7d193a942c4c53888bdb2de3d26f",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "b9662310a75fd3b7a4702077fb434a56",
-"/": "b9662310a75fd3b7a4702077fb434a56",
-"main.dart.js": "01183b01ba30ecfed8cac2c57af309f3",
-"manifest.json": "e62ca04c9a04b04227494232a88ade2e"
+"index.html": "4e8378f8caefa888084d30c5368604ec",
+"/": "4e8378f8caefa888084d30c5368604ec",
+"main.dart.js": "1b79882206b55378596e3d12b40f5083",
+"manifest.json": "bb75104b3c6d5217a82fd44ac2dd5b2c"
 };
 
 self.addEventListener('activate', function (event) {
