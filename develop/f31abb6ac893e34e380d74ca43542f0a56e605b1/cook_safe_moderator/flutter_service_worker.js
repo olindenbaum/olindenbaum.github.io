9ba516bd0ea5fa1e8fbdf6ffc43c5b6d5f424e62
oscarlindenbaum <56974866+oscarlindenbaum@users.noreply.github.com>
2020-04-25 17:53:49 +0100

    moderator build search past records, build 3.4.6+0

--- develop/f31abb6ac893e34e380d74ca43542f0a56e605b1/cook_safe_moderator/flutter_service_worker.js
+++ develop/f31abb6ac893e34e380d74ca43542f0a56e605b1/cook_safe_moderator/flutter_service_worker.js
@@ -6,14 +6,14 @@ const RESOURCES = {
 "assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
 "assets/images/defaultAvatar.jpg": "d5f51047d8bd6327ec4a74361a7aae7f",
 "assets/images/favicon.ico": "6fa40b18241f63bc08805ac763ac91b9",
-"assets/LICENSE": "61ff1e3dd9312f20e491dfa1cef51b38",
+"assets/LICENSE": "d4136f21f92ea0a088dedc623527bb92",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
 "favicon.png": "100b7d193a942c4c53888bdb2de3d26f",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "4e8378f8caefa888084d30c5368604ec",
 "/": "4e8378f8caefa888084d30c5368604ec",
-"main.dart.js": "b0cb77b9b5d5f4286b21cacfaa609c60",
+"main.dart.js": "0e714363f3d7c46c00d61b382a1a29ec",
 "manifest.json": "bb75104b3c6d5217a82fd44ac2dd5b2c"
 };
 
