aaa198dee4cdc2162a3542483c2a33769dc8a551
oscarlindenbaum <56974866+oscarlindenbaum@users.noreply.github.com>
2020-04-29 15:04:58 +0100

    fixed CORS error and added error handling, more reliable

--- develop/f31abb6ac893e34e380d74ca43542f0a56e605b1/cook_safe_moderator/oldversion/flutter_service_worker.js
+++ develop/f31abb6ac893e34e380d74ca43542f0a56e605b1/cook_safe_moderator/oldversion/flutter_service_worker.js
@@ -1,19 +1,22 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "9deffc3895ad49ed383b0f3fb108956f",
-"assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
+  "assets/AssetManifest.json": "e81a5722fc1b885850632432027c92f6",
+"assets/FontManifest.json": "18eda8e36dfa64f14878d07846d6e17f",
 "assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
 "assets/images/defaultAvatar.jpg": "d5f51047d8bd6327ec4a74361a7aae7f",
 "assets/images/favicon.ico": "6fa40b18241f63bc08805ac763ac91b9",
-"assets/LICENSE": "d4136f21f92ea0a088dedc623527bb92",
+"assets/LICENSE": "80bbd3f7f4abe5c68f3f9315007da0ca",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
+"assets/packages/font_awesome_flutter/lib/fonts/fa-brands-400.ttf": "5a37ae808cf9f652198acde612b5328d",
+"assets/packages/font_awesome_flutter/lib/fonts/fa-regular-400.ttf": "2bca5ec802e40d3f4b60343e346cedde",
+"assets/packages/font_awesome_flutter/lib/fonts/fa-solid-900.ttf": "2aa350bd2aeab88b601a593f793734c0",
 "favicon.png": "100b7d193a942c4c53888bdb2de3d26f",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "4e8378f8caefa888084d30c5368604ec",
 "/": "4e8378f8caefa888084d30c5368604ec",
-"main.dart.js": "0e714363f3d7c46c00d61b382a1a29ec",
+"main.dart.js": "c18c9799380e496a3270ae7a20983e39",
 "manifest.json": "bb75104b3c6d5217a82fd44ac2dd5b2c"
 };
 
