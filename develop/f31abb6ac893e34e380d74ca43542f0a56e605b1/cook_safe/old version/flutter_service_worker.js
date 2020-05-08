535624c4e990183161ca0b098cc3560aad64c3a7
oscarlindenbaum <56974866+oscarlindenbaum@users.noreply.github.com>
2020-05-08 10:48:57 +0100

    repush web user

--- develop/f31abb6ac893e34e380d74ca43542f0a56e605b1/cook_safe/old version/flutter_service_worker.js
+++ develop/f31abb6ac893e34e380d74ca43542f0a56e605b1/cook_safe/old version/flutter_service_worker.js
@@ -7,7 +7,7 @@ const RESOURCES = {
 "assets/assets/recipes.json": "2183b454e8b76ef0ddbe27ae1095d094",
 "assets/FontManifest.json": "18eda8e36dfa64f14878d07846d6e17f",
 "assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"assets/LICENSE": "28537fd7a6416eb851aebb6f8ee6896a",
+"assets/LICENSE": "5151d6d9544a147bddb6c1fb4a1e11fa",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
 "assets/packages/font_awesome_flutter/lib/fonts/fa-brands-400.ttf": "5a37ae808cf9f652198acde612b5328d",
 "assets/packages/font_awesome_flutter/lib/fonts/fa-regular-400.ttf": "2bca5ec802e40d3f4b60343e346cedde",
@@ -17,7 +17,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "b9662310a75fd3b7a4702077fb434a56",
 "/": "b9662310a75fd3b7a4702077fb434a56",
-"main.dart.js": "9e167539bbb817ad7797e2749b769675",
+"main.dart.js": "24de46071beb3737a0e2a676db5e33ad",
 "manifest.json": "e62ca04c9a04b04227494232a88ade2e"
 };
 
