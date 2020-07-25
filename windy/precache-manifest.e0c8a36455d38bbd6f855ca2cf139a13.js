c8c3fb592b335ecfabe917e92b51b6f2c9e1621f
Oscar <oscarlindenbaum@gmail.com>
2020-07-25 17:20:45 +0100

    adding windy

---
+++ windy/precache-manifest.e0c8a36455d38bbd6f855ca2cf139a13.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b098b23b1ba03aab8e5330fc5c39090a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "800fecaad6ff8b843a90",
+    "url": "/static/css/main.839b559c.chunk.css"
+  },
+  {
+    "revision": "c47617d80c44acc9ff5a",
+    "url": "/static/js/2.7c3b5ed7.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.7c3b5ed7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "800fecaad6ff8b843a90",
+    "url": "/static/js/main.fce80dfd.chunk.js"
+  },
+  {
+    "revision": "0ddfd44cc969fc3af1a9",
+    "url": "/static/js/runtime-main.9f0abd4e.js"
+  },
+  {
+    "revision": "5b07bcd19ac35e90bc17a91d9c85cd0c",
+    "url": "/static/media/pog.5b07bcd1.png"
+  }
+]);
